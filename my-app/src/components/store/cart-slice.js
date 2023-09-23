45c078f1d66353bdf816868b2783c642e492eb01
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-09-23 16:44:00 +0000

    update cart in the product

--- my-app/src/components/store/cart-slice.js
+++ my-app/src/components/store/cart-slice.js
@@ -1,18 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-    name: cart,
+    name: 'cart',
     initialState: {
         items: [],
         totalQuantity: 0,
-        totalAmount: 0
     },
     reducers: {
         //action arg carries extra info once dispatched
         //payload prop contain any extra data you add to action
         //payload property sets by redux toolkit
         addItemTocart(state, action){
-            const item = action.payload;
+            const newItem = action.payload;
             const existingItem = state.items.find((item) => item.id === newItem.id);
             if(!existingItem){
                 state.items.push({
@@ -27,7 +26,19 @@ const cartSlice = createSlice({
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
         },
-        removeItemFromCart(){}
+        removeItemFromCart(state, action){
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            if(existingItem.quantity === 1){
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        }
     }
 });
 
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
