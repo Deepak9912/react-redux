b6d078a24dd441f59f4a95a83e3b51f4c983e6ca
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-09-26 19:52:20 +0000

    final commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData, fetchCartData } from './store/cart-action';
 
 let isInitial = true;
 
@@ -18,11 +18,18 @@ function App() {
   const notification = useSelector((state) => state.notification);
   //whenever redux store change, below component is executed, and we get latest state
   
+  useEffect(()=> {
+    dispatch(fetchCartData());
+  }, [dispatch])
   
   useEffect(() => {
     if(!isInitial){
       isInitial = false;
       return;
+    }
+
+    if(cart.changed){
+      dispatch(sendCartData(cart));
     }
 
     dispatch(sendCartData(cart));
